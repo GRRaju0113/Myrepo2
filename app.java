4a53253c869f5f0d6a4523dd57a7b6a62208a854
root <root@ip-172-31-23-112.ec2.internal>
2025-11-09 11:41:41 +0000

    java code newly added

---
+++ app.java
@@ -0,0 +1,33 @@
+public class HelloWorld {
+    
+    // Your program begins with a call to main()
+    public static void main(String[] args)
+    {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+}public class HelloWorld {
+    
+    // Your program begins with a call to main()
+    public static void main(String[] args)
+    {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+}public class HelloWorld {
+    
+    // Your program begins with a call to main()
+    public static void main(String[] args)
+    {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+}public class HelloWorld {
+    
+    // Your program begins with a call to main()
+    public static void main(String[] args)
+    {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+}
