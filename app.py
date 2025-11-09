6ed2e32f0776ac1cd3ed8a66d75d555dcadccdbf
root <root@ip-172-31-23-112.ec2.internal>
2025-11-09 12:17:12 +0000

    added data

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
-<!DOCTYPE html>
+y>
+    <h1>Welcome to My Page!</h1>
+    <p>This is a simple paragraph demonstrating HTML structure.</p>
+    <p>You can add more content here, like images, links, or lists.</p>
+</body>
+</html>y>
+    <h1>Welcome to My Page!</h1>
+    <p>This is a simple paragraph demonstrating HTML structure.</p>
+    <p>You can add more content here, like images, links, or lists.</p>
+</body>
+</html:wq
+><!DOCTYPE html>
 <html>
 <head>
     <title>My First Web Page</title>
